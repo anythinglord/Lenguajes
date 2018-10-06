42433ff1dca0a3670579805ac4cd9e3748d6473a
Jesus Salcedo <sjsalcedoa@unal.edu.co>
2018-10-06 10:08:21 -0500

    Sintactico(primeros & siguientes)

---
+++ TestClass2.1.java
@@ -0,0 +1,252 @@
+
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+//import java.util.Iterator;
+
+
+
+public class Sintax {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static HashMap<String,ArrayList<ArrayList<String>>> Gramatica = new HashMap<String,ArrayList<ArrayList<String>>>();
+    public static HashMap<String,ArrayList<String>> First = new HashMap<String,ArrayList<String>>();
+    public static HashMap<String,ArrayList<String>> Next = new HashMap<String,ArrayList<String>>();
+    public static ArrayList<String> keys = new ArrayList<String>();
+
+
+
+    public static void fillG(){
+        ArrayList<ArrayList<String>> rules = new ArrayList<ArrayList<String>>();
+        ArrayList<String> rul = new ArrayList<String>();
+
+        rul.add("B");rul.add("C");rules.add(rul);
+        rul = new ArrayList<String>();
+
+        rul.add("ant");rul.add("A");rul.add("all");rules.add(rul);
+        rul = new ArrayList<String>();
+
+        rul.add("big");rul.add("C");rules.add(rul);
+        Gramatica.put("A", rules);keys.add("A");
+
+        rul = new ArrayList<String>();rules = new ArrayList<ArrayList<String>>();
+
+        rul.add("big");rul.add("C");rules.add(rul);
+        rul = new ArrayList<String>();
+
+        rul.add("bus");rul.add("A");rul.add("boss");rules.add(rul);
+        rul = new ArrayList<String>();
+
+        rul.add(" ");rules.add(rul);
+        Gramatica.put("B", rules);keys.add("B");
+
+        rul = new ArrayList<String>();rules = new ArrayList<ArrayList<String>>();
+
+        rul.add("cat");rules.add(rul);
+        rul = new ArrayList<String>();
+
+        rul.add("cow");rules.add(rul);
+        Gramatica.put("C", rules);keys.add("C");
+    }
+
+    public static void showH(HashMap<String,ArrayList<ArrayList<String>>> a){
+        for (int i = 0; i < a.size(); i++) {
+            System.out.println("No Teminal: "+keys.get(i));
+            for (int j = 0; j < a.get(keys.get(i)).size(); j++) {
+                for (int k = 0; k < a.get(keys.get(i)).get(j).size() ; k++) {
+                    System.out.print(" "+a.get(keys.get(i)).get(j).get(k));
+                }
+                System.out.println();
+            }
+        }
+    }
+
+    public static void showHA(HashMap<String,ArrayList<String>> a){
+        String key ;
+        for (int i = 0; i < a.size() ; i++) {
+            key = keys.get(i);
+            for (int j = 0; j < a.get(key).size() ; j++) {
+                System.out.println(key+": "+a.get(key).get(j));
+            }
+        }
+    }
+
+    public static ArrayList join(ArrayList a, ArrayList b){
+        ArrayList c = new ArrayList<String>();
+        for (int i = 0; i < a.size(); i++)
+            c.add(a.get(i));
+
+        for (int i = 0; i < b.size(); i++)
+            c.add(b.get(i));
+
+        return c;
+    }
+
+    public static ArrayList CalFirst(String key){
+
+        String rule,rule2 ;
+        ArrayList<String> elem = new ArrayList<String>();
+        ArrayList<String> aux = new ArrayList<String>();
+
+        // recorre todas las producciones del no terminal key 
+        for (int i = 0; i < Gramatica.get(key).size(); i++) {
+        	// recorre cada caracter de cada produccion
+            rule = String.valueOf(Gramatica.get(key).get(i).get(0));
+            //System.out.println("rule: "+rule);
+        	// si es no terminal
+            if (keys.contains(rule) == false){
+                elem.add(rule);
+            }else {
+                //System.out.println("Cal First(for else)of_ "+rule);
+                aux = CalFirst(rule);
+
+                if (aux.contains(" ")) {
+                    elem = join(elem,aux);
+            		aux = new ArrayList<String>();
+            		for (int j = 1; j < Gramatica.get(key).get(i).size(); j++) {
+            			rule2 = String.valueOf(Gramatica.get(key).get(i).get(j));
+            			if (keys.contains(rule2)==true){
+            				aux = CalFirst(rule2);
+                        }else {
+            				aux.add(rule2);
+            				break;
+            			}
+            		}
+            	}
+            	elem = join(elem,aux) ;
+            }
+
+        }
+        return elem;
+    }
+
+
+    public static ArrayList CalNext(String key){
+
+        String em,em1=" ";
+        ArrayList<String> elem = new ArrayList<String>();
+        ArrayList<String> aux = new ArrayList<String>();
+        int tam = 0 ;
+
+        for (int i = 0; i < Gramatica.size() ; i++) {
+
+            for (int j = 0; j < Gramatica.get(keys.get(i)).size()  ; j++) {
+                tam = Gramatica.get(keys.get(i)).get(j).size();
+                //System.out.println((j+1)+" "+tam);
+                for (int k = 0; k < tam ; k++) {
+                    em = Gramatica.get(keys.get(i)).get(j).get(k);
+                    //System.out.println("em: "+em);
+                    if (k < tam-1){
+                        em1 = Gramatica.get(keys.get(i)).get(j).get(k+1);
+                        //System.out.println("em1: "+em1);
+                    }
+
+                    if(em == key ){
+                        if(k == tam-1){
+                            aux = CalNext(keys.get(i));
+                            elem = join(aux,elem);
+                        }else{
+                            // si es un No terminal
+                            if(keys.contains(em1) == true){
+
+                                elem = join(First.get(em1),elem);
+                                if (First.get(em1).contains(" ")){
+                                    elem = join(aux,elem);
+                                }
+                            }else{
+                                elem.add(em1);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return elem;
+    }
+
+    public static HashMap UnicoN(HashMap<String,ArrayList<String>> a){
+        String key ;
+        HashSet<String> hashSet;
+        for (int i = 0; i < a.size() ; i++) {
+            key = keys.get(i);
+            hashSet = new HashSet<String>(a.get(key));
+            a.get(key).clear();
+            a.get(key).addAll(hashSet);
+
+        }
+        return a;
+    }
+    // para saber si esta epsilon en una de las producciones
+    public static boolean exE(String key){
+        boolean ans = false;
+        for (int i = 0; i < Gramatica.get(key).size() ; i++) {
+            for (int j = 0; j < Gramatica.get(key).get(i).size() ; j++) {
+                if(Gramatica.get(key).get(i).contains(" ")){
+                    ans = true;
+                }
+            }
+        }
+        return ans;
+    }
+
+    public static HashMap quitE(HashMap<String,ArrayList<String>> a){
+        String key ;
+        String val;
+        ArrayList<String> temp;
+        for (int i = 0; i < a.size() ; i++) {
+            key = keys.get(i);
+            for (int j = 0; j < a.get(key).size() ; j++) {
+                val = a.get(key).get(j);
+                if(val.equals(" ") && exE(key) == false)
+                    a.get(key).remove(val);
+
+            }
+        }
+        return a;
+    }
+    public static void main(String[] args) {
+
+        Sintax s = new Sintax();
+        ArrayList<String> elem ;
+        s.fillG();
+        // Muestra la gramatica
+        s.showH(Gramatica);
+
+        //Calcular primeros por cada No terminal
+        for (int i = 0; i < Gramatica.size(); i++) {
+
+            elem = new ArrayList<String>();
+            //System.out.println("llave: "+keys.get(i));
+            elem = s.CalFirst(keys.get(i));
+            First.put(keys.get(i),elem);
+
+        }
+        //retira los repetidos
+        First = s.UnicoN(First);
+        //retira epsilon
+        First = s.quitE(First);
+
+        // Mostrar primeros
+        System.out.println("FIRST");
+        s.showHA(First);
+
+        //Calcular siguientes por cada No terminal
+        for (int i = 0; i < Gramatica.size(); i++) {
+            elem = new ArrayList<String>();
+            elem = s.CalNext(keys.get(i));
+            Next.put(keys.get(i),elem);
+
+        }
+
+        // Mostrar siguientes
+        System.out.println("NEXT");
+        Next = s.UnicoN(Next);
+        s.showHA(Next);
+
+
+    }
+    
+}
